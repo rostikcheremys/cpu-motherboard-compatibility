74c5d54ab1da90db806ce503866f861a28907d52
rostikcheremys <rostikcheremys@gmail.com>
2025-05-04 18:59:06 +0300

    add: sunIcon and moonIcon

--- client/src/components/ThemeSwitch/ThemeSwitch.jsx
+++ client/src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -1,6 +1,16 @@
 import { styled } from '@mui/material/styles';
 import Switch from '@mui/material/Switch';
 
+const sunIcon = encodeURIComponent(`
+  <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20">
+    <path fill="#FDB813" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/>
+  </svg>`);
+
+const moonIcon = encodeURIComponent(`
+  <svg xmlns='http://www.w3.org/2000/svg' height='20' width='20' viewBox='0 0 20 20'>
+    <path fill='#FFD700' d='M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z'/>
+  </svg>`);
+
 export const ThemeSwitch = styled(Switch)(() => ({
     width: 62,
     height: 34,
@@ -13,7 +23,7 @@ export const ThemeSwitch = styled(Switch)(() => ({
             color: '#fff',
             transform: 'translateX(22px)',
             '& .MuiSwitch-thumb:before': {
-                backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' height='20' width='20' viewBox='0 0 20 20'><path fill='white' d='M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z'/></svg>")`,
+                backgroundImage: `url("data:image/svg+xml;utf8,${moonIcon}")`,
             },
             '& + .MuiSwitch-track': {
                 backgroundColor: '#8796A5',
@@ -22,7 +32,7 @@ export const ThemeSwitch = styled(Switch)(() => ({
         },
     },
     '& .MuiSwitch-thumb': {
-        backgroundColor: '#003892',
+        backgroundColor: '#7d8183',
         width: 32,
         height: 32,
         '&::before': {
@@ -32,11 +42,11 @@ export const ThemeSwitch = styled(Switch)(() => ({
             height: '100%',
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
-            backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' height='20' width='20' viewBox='0 0 20 20'><path fill='white' d='M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z'/></svg>")`,
+            backgroundImage: `url("data:image/svg+xml;utf8,${sunIcon}")`,
         },
     },
     '& .MuiSwitch-track': {
-        borderRadius: 20 / 2,
+        borderRadius: 10,
         backgroundColor: '#aab4be',
         opacity: 1,
     },
